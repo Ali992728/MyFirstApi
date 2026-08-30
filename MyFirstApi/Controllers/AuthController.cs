9b368a73fc7b6a58f5eb82d771a91c05430d0dab
Ali Abrar Khan <abrar9927@gmail.com>
2026-08-31 02:20:29 +0600

    postman tested

--- MyFirstApi/Controllers/AuthController.cs
+++ MyFirstApi/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyFirstApi.Dto;
+using MyFirstApi.IService;
 
 namespace MyFirstApi.Controllers
 {
@@ -7,5 +9,34 @@ namespace MyFirstApi.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        public readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody]UserDto userDto)
+        {
+            try
+            {
+                var result = await _authService.LoginUser(userDto);
+                if(result.Item1==0)
+                {
+                    return NotFound(result.Item2);
+                }
+                if(result.Item1==1)
+                {
+                    return BadRequest(result.Item2);
+                }
+                
+                return Ok(result.Item2);
+                
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
     }
 }
